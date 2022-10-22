4ab1fa17e2f7b56d6794af5c5e7b7011d397592c
LevitD <diml71@mail.ru>
2022-10-22 20:33:18 +0300

    Ex_003 sum

---
+++ Ex_003_sum/Program.cs
@@ -0,0 +1,6 @@
+﻿// Сумма 2 чисел
+int numberA = 3;
+int numberB = 50;
+int result = numberA + numberB;
+
+Console.WriteLine(result);
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 dotnet new console - создать новый проект
 
 dotnet run - запустить проект
+
+Console.Write
+Console.ReadLine()
