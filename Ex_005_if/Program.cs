c93459358fa0b03f0788e29c5f51303d9a45d87a
LevitD <diml71@mail.ru>
2022-10-22 20:51:46 +0300

    Ex_005 любимчик

---
+++ Ex_005_if/Program.cs
@@ -0,0 +1,10 @@
+﻿// SIF
+Console.Write("Введите имя пользователя:");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "dim") {
+    Console.Write("Ура, это же Dim!");
+} else {
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
