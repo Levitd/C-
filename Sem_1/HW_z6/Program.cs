183a82c6773010a4fdb4d41cc9bc9b85e0b07cef
LevitD <diml71@mail.ru>
2022-10-23 15:15:21 +0300

    Четное или нечетное число

---
+++ Sem_1/HW_z6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int number1 = int.Parse(Console.ReadLine());
+if (GetEvenNumber(number1))
+{
+    Console.WriteLine($"{number1} - число четное");
+}
+else
+{
+    Console.WriteLine($"{number1} - число Нечетное");
+}
+
+static bool GetEvenNumber(int num)
+{
+    if (num % 2 == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
