22dfc20fb32bd0c7e7d895aa189839a5c95970dc
LevitD <diml71@mail.ru>
2022-10-23 15:03:40 +0300

    максимальное из 3 чисел

---
+++ Sem_1/HW_z4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+Console.Clear();
+
+Console.Write("Введите Первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите Второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите Третье число: ");
+int number3 = int.Parse(Console.ReadLine());
+
+int max;
+
+if (number1 > number2)
+{
+    max = number1;
+}
+else if (number1 < number2)
+{
+    max = number2;
+}
+else
+{
+    max = number2;
+}
+if (number3 > max)
+{
+    max = number3;
+}
+
+Console.WriteLine($"Максимальное из введенных чисел: {max}");
+
