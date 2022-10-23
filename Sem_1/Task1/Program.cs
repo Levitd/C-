f133bf890714b8ba8396824aa60fe8d4474d33a9
LevitD <diml71@mail.ru>
2022-10-23 14:09:59 +0300

    проверка квадрата числа

---
+++ Sem_1/Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите Первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите Второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+
+int sqr = Convert.ToInt32(Math.Pow(number1,2));
+
+if (sqr == number2) {
+    Console.WriteLine($"Правильно, число {number2} является квадратом числа {number1}");
+} else {
+    Console.WriteLine($"Не Правильно!!! Число {number2} Не является квадратом числа {number1}");
+    Console.WriteLine($"Квадрат числа {number1} будет  {sqr}");
+}
