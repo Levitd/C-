62ab08e5debd64dc63050224147797e509028fab
LevitD <diml71@mail.ru>
2022-10-23 14:56:14 +0300

    Какое число больше

---
+++ Sem_1/HW_z2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+Console.Clear();
+
+Console.Write("Введите Первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите Второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+
+int min =0, max=0, rav=0;
+
+if (number1>number2) {
+    min=number2;
+    max=number1;
+} else if (number1<number2) {
+    min=number1;
+    max=number2;
+} else {
+    rav=1;
+}
+if (rav == 1){
+    Console.WriteLine("Введенные числа равны!");
+} else {
+    Console.WriteLine($"Число {max} больше чем {min}");
+}
