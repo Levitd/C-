d343a994b16dd5d73e3750c8e9100f636afc5126
LevitD <diml71@mail.ru>
2022-10-23 14:37:25 +0300

    последовательность чисел

---
+++ Sem_1/Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+Console.Write("Введите Число: ");
+int number = int.Parse(Console.ReadLine());
+int startNumber = -number;
+
+Console.WriteLine("");
+while (startNumber <= number){
+    if (startNumber != -number){
+        Console.Write(", ");
+    }
+    Console.Write(startNumber);
+    startNumber++;
+}
