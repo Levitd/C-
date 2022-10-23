b722ddaf51849e3546718f244c74ff9abe6ff51a
LevitD <diml71@mail.ru>
2022-10-23 14:01:33 +0300

    Task 0

---
+++ Sem_1/Task0/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int sqr = number * number;
+int sqr2 = Convert.ToInt32(Math.Pow(number,2));
+Console.WriteLine($"Квадрат числа = {sqr}");
+Console.WriteLine($"Квадрат числа = {sqr2}");
