5fa2397961cfc18aa9adcfff3e3b72d6a024c66d
LevitD <diml71@mail.ru>
2022-10-23 14:45:13 +0300

    Последнее число их з-х значного числа

---
+++ Sem_1/Task4/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+Console.Clear();
+
+Console.Write("Введите Трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+int ostatok = number % 10;
+
+Console.WriteLine($"Последнее число: {ostatok}");
