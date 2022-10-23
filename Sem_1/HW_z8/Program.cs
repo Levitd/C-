7fe00aa13e9279c1cc612d5a77c533ee4f84eed0
LevitD <diml71@mail.ru>
2022-10-23 15:20:12 +0300

    Показть четноые числа от 1 до N

---
+++ Sem_1/HW_z8/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+Console.Write("Введите Число: ");
+int number = int.Parse(Console.ReadLine());
+int startNumber = 2;
+
+Console.WriteLine("");
+while (startNumber <= number)
+{
+    if (startNumber != 2)
+    {
+        Console.Write(", ");
+    }
+    Console.Write(startNumber);
+    startNumber = startNumber + 2;
+}
