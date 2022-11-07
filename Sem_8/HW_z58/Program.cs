143b2851e34bd52085d00e16e90091661fb1491d
LevitD <diml71@mail.ru>
2022-11-07 14:01:52 +0300

    seminar 8 day 2

---
+++ Sem_8/HW_z58/Program.cs
@@ -0,0 +1,79 @@
+﻿// Задача 58
+// Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц
+
+using System.Text.RegularExpressions;
+using System;
+using static System.Console;
+using System.Data;
+
+Clear();
+try
+{
+    Write("Введите размер массивов через пробел: ");
+    string[] nums = ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+    int minElement = 0;
+    int maxElement = 10;
+    int[,] array1 = GetArray(int.Parse(nums[0]), int.Parse(nums[1]), minElement, maxElement);
+    int[,] array2 = GetArray(int.Parse(nums[0]), int.Parse(nums[1]), minElement, maxElement);
+
+    WriteLine("Массив 1: ");
+    PrintArray(array1);
+    WriteLine("Массив 2: ");
+    PrintArray(array2);
+    WriteLine("Произведение 1 и 2 массивов: ");
+    int[,] GenerationArray = GetGenerationArray(array1, array2);
+
+    PrintArray(GenerationArray);
+    
+}
+catch (OverflowException)
+{
+    Write("Переполнение! Ввели слишком большое число!");
+}
+catch (FormatException)
+{
+    Write("Ожидалось число! Ввели непонять что!");
+}
+catch
+{
+    Write("Ошибка выполнения!");
+}
+int[,] GetGenerationArray(int[,] inArray1, int[,] inArray2)
+{
+    int[,] result = new int[inArray1.GetLength(0), inArray1.GetLength(1)];
+    for (int i = 0; i < inArray1.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray1.GetLength(1); j++)
+        {
+            result[i,j] = inArray1[i, j] * inArray2[i, j];
+        }
+    }
+    return result;
+}
+
+//Создание двумерного массива 
+int[,] GetArray(int rows, int colums, int min, int max)
+{
+    int[,] result = new int[rows, colums];
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return result;
+}
+//Печать двумерного массива
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j].ToString().PadLeft(5, ' ')} ");
+        }
+        WriteLine();
+    }
+}
---
+++ Sem_8/HW_z60/Program.cs
@@ -0,0 +1,100 @@
+﻿using System.Diagnostics;
+// Задача 60
+// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+
+using System.Text.RegularExpressions;
+using System;
+using static System.Console;
+using System.Data;
+using System.Diagnostics;
+{
+    
+}
+
+Clear();
+try
+{
+    Write("Введите размер Трехмерного массива через пробел: ");
+    string[] nums = ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+    int minElement = 10;
+    int maxElement = 99;
+    int notRepeat = 1;
+    if (notRepeat == 1)
+    {
+        int countElemen = int.Parse(nums[0]) * int.Parse(nums[1]) * int.Parse(nums[2]);
+        if (countElemen > (maxElement - minElement))
+        {
+            WriteLine("Создаваемый массив больше чем заданное кол-во уникальных элементов");
+            return;
+        }
+    }
+    Stopwatch stopwatch = new Stopwatch();
+    stopwatch.Start();
+
+    int[,,] array = GetArray3x(int.Parse(nums[0]), int.Parse(nums[1]), int.Parse(nums[2]), minElement, maxElement, notRepeat);
+
+    PrintArray3x(array);
+
+    stopwatch.Stop();
+    Console.WriteLine($"Затраченное время: {1000L*1000L*stopwatch.ElapsedTicks/Stopwatch.Frequency}(мкс).");
+
+
+}
+catch (OverflowException)
+{
+    Write("Переполнение! Ввели слишком большое число!");
+}
+catch (FormatException)
+{
+    Write("Ожидалось число! Ввели непонять что!");
+}
+catch
+{
+    Write("Ошибка выполнения!");
+}
+
+//Создание трехмерного массива 
+int[,,] GetArray3x(int x, int y, int z, int min, int max, int notRepeat)
+{
+    int[,,] result = new int[x, y, z];
+    string el = string.Empty;
+    int foundEl = -1;
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            for (int k = 0; k < result.GetLength(2); k++)
+            {
+                while (true)
+                {
+                    result[i, j, k] = new Random().Next(min, max + 1);
+                    if (notRepeat == 1)
+                    {
+                        foundEl = el.IndexOf(result[i, j, k].ToString());
+                        if (foundEl == -1) break;
+                    }
+                    else break;
+                }
+                el += result[i, j, k].ToString() + ", ";
+            }
+        }
+    }
+    return result;
+}
+
+//Печать трехмерного массива
+void PrintArray3x(int[,,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            for (int k = 0; k < inArray.GetLength(2); k++)
+            {
+                Write($"{inArray[i, j, k].ToString().PadLeft(2, ' ')}({i},{j},{k}) ");
+            }
+        }
+        WriteLine();
+    }
+}
